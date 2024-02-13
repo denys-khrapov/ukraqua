dac1aa1ea13c8feee008a59c111e05cc8834f0fb
Khrapov Denys <khrapov.denys@gmail.com>
2024-02-13 12:08:05 +0200

    created base structure

---
+++ src/js/files/script.js
@@ -0,0 +1,11 @@
+// Підключення функціоналу "Чертоги Фрілансера"
+// Підключення списку активних модулів
+
+
+import {main} from './main.js';
+import {additional} from './additional.js';
+
+document.addEventListener('DOMContentLoaded', function () {
+	main();
+	additional();
+});
