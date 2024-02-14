c60a76e3f4d2be6638724a4fd2308138028f8320
Oleh Fedorov <akademuk24@gmail.com>
2024-02-14 11:07:45 +0200

    seo section

--- src/js/files/additional.js
+++ src/js/files/additional.js
@@ -1,16 +1,37 @@
 export function additional() {
-    const swiper = new Swiper('.reviews__swiper', {
+    // const swiper = new Swiper('.reviews__swiper', {
         
-        loop: true,
+    //     loop: true,
 
-        pagination: {
-          el: '.reviews-pagination',
-          type: 'bullets'
-        },
+    //     pagination: {
+    //       el: '.reviews-pagination',
+    //       type: 'bullets'
+    //     },
       
-        navigation: {
-          nextEl: '.reviews-button-next',
-          prevEl: '.reviews-button-prev',
-        },
-      });
+    //     navigation: {
+    //       nextEl: '.reviews-button-next',
+    //       prevEl: '.reviews-button-prev',
+    //     },
+    //   });
+
+
+
+
+    var container = document.getElementById("seo__container");
+    var button = document.getElementById("seo__btn");
+  
+    button.addEventListener("click", function () {
+      if (container.style.maxHeight) {
+        container.style.maxHeight = null;
+        button.innerHTML = "Розгорнути текст";
+        button.classList.add("collapsed");
+      } else {
+        container.style.maxHeight = container.scrollHeight + "px";
+        button.innerHTML = "Згорнути текст";
+        button.classList.remove("collapsed");
+      }
+    });
+    
+      
+      
 }
