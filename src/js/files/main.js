bf954a0dd5119a7dde37b6519f04a8a45ea8bf2d
Khrapov Denys <khrapov.denys@gmail.com>
2024-02-14 13:15:47 +0200

    added water-care section and styles

--- src/js/files/main.js
+++ src/js/files/main.js
@@ -1,3 +1,22 @@
 export function main() {
+	initSwiper()
 
+
+	function initSwiper() {
+		let swiperWaterCare = new Swiper('.slider-water-care', {
+			slidesPerView: 'auto',
+			spaceBetween: 24,
+			loop: false,
+			speed: 600,
+			navigation: {
+				nextEl: '.slider-water-care__nav .swiper-button-next',
+				prevEl: '.slider-water-care__nav .swiper-button-prev'
+			},
+			pagination: {
+				el: '.slider-water-care__nav .swiper-pagination',
+				clickable: true
+
+			}
+		})
+	}
 }
