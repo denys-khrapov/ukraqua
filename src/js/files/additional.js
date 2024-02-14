fdd8fb770101398b7027c74cd910d4d6b729abbf
Oleh Fedorov <akademuk24@gmail.com>
2024-02-14 16:17:05 +0200

    footer section

--- src/js/files/additional.js
+++ src/js/files/additional.js
@@ -1,37 +1,50 @@
 export function additional() {
-    // const swiper = new Swiper('.reviews__swiper', {
-        
-    //     loop: true,
+  // const swiper = new Swiper('.reviews__swiper', {
 
-    //     pagination: {
-    //       el: '.reviews-pagination',
-    //       type: 'bullets'
-    //     },
-      
-    //     navigation: {
-    //       nextEl: '.reviews-button-next',
-    //       prevEl: '.reviews-button-prev',
-    //     },
-    //   });
+  //     loop: true,
 
+  //     pagination: {
+  //       el: '.reviews-pagination',
+  //       type: 'bullets'
+  //     },
 
+  //     navigation: {
+  //       nextEl: '.reviews-button-next',
+  //       prevEl: '.reviews-button-prev',
+  //     },
+  //   });
 
 
-    var container = document.getElementById("seo__container");
-    var button = document.getElementById("seo__btn");
-  
-    button.addEventListener("click", function () {
-      if (container.style.maxHeight) {
-        container.style.maxHeight = null;
-        button.innerHTML = "Розгорнути текст";
-        button.classList.add("collapsed");
-      } else {
-        container.style.maxHeight = container.scrollHeight + "px";
-        button.innerHTML = "Згорнути текст";
-        button.classList.remove("collapsed");
-      }
+
+  // SEO SECTION раскрытие блока
+  var container = document.getElementById("seo__container");
+  var button = document.getElementById("seo__btn");
+
+  button.addEventListener("click", function () {
+    if (container.style.maxHeight) {
+      container.style.maxHeight = null;
+      button.innerHTML = "Розгорнути текст";
+      button.classList.add("collapsed");
+    } else {
+      container.style.maxHeight = container.scrollHeight + "px";
+      button.innerHTML = "Згорнути текст";
+      button.classList.remove("collapsed");
+    }
+  });
+
+  // FOOTER SECTION акардион локации
+  $(document).ready(function () {
+    $('.accordion-header').click(function () {
+      $(this).next('.accordion-content').slideToggle();
+      $(this).toggleClass('active');
+      $('.accordion-header').not(this).removeClass('active');
     });
-    
-      
-      
+  });
+
+  // FOOTER SECTION список телефонов
+  $(document).ready(function () {
+    $('.click__address').click(function () {
+      $('.click__address-container').toggleClass('expanded');
+    });
+  });
 }
