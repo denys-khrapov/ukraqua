96fd786aa3e7eb8205b04604ba3126173c691cc8
Oleh Fedorov <akademuk24@gmail.com>
2024-02-14 09:40:48 +0200

    reviews section

--- src/js/files/additional.js
+++ src/js/files/additional.js
@@ -1,3 +1,16 @@
 export function additional() {
+    const swiper = new Swiper('.reviews__swiper', {
+        
+        loop: true,
 
+        pagination: {
+          el: '.reviews-pagination',
+          type: 'bullets'
+        },
+      
+        navigation: {
+          nextEl: '.reviews-button-next',
+          prevEl: '.reviews-button-prev',
+        },
+      });
 }
