5132bba5a8d49f5fd5e2dbbfadbd435cdf92310a
Khrapov Denys <khrapov.denys@gmail.com>
2024-02-16 13:40:52 +0200

    added header section and styles

--- src/js/files/additional.js
+++ src/js/files/additional.js
@@ -1,98 +1,97 @@
 export function additional() {
-  reviewsSwiper()
+	reviewsSwiper();
 
 
-  function reviewsSwiper() {
-    let swiperReviews = new Swiper('.reviews__swiper', {
-      slidesPerView: 'auto',
-      spaceBetween: 24,
-      loop: false,
-      speed: 600,
-      navigation: {
-        nextEl: '.reviews__navigation .reviews-button-next',
-        prevEl: '.reviews__navigation .reviews-button-prev'
-      },
-      pagination: {
-        el: '.reviews__navigation .reviews-pagination',
-        clickable: true
-      }
-    })
-  }
+	function reviewsSwiper() {
+		let swiperReviews = new Swiper('.reviews__swiper', {
+			slidesPerView: 'auto',
+			spaceBetween: 24,
+			loop: false,
+			speed: 600,
+			navigation: {
+				nextEl: '.reviews__navigation .reviews-button-next',
+				prevEl: '.reviews__navigation .reviews-button-prev'
+			},
+			pagination: {
+				el: '.reviews__navigation .reviews-pagination',
+				clickable: true
+			}
+		});
+	}
 
 
+	// SEO SECTION раскрытие блока
+	var container = document.getElementById('seo__container');
+	var button = document.getElementById('seo__btn');
 
-  // SEO SECTION раскрытие блока
-  var container = document.getElementById("seo__container");
-  var button = document.getElementById("seo__btn");
+	button.addEventListener('click', function() {
+		if (container.style.maxHeight) {
+			container.style.maxHeight = null;
+			button.innerHTML = 'Розгорнути текст';
+			button.classList.add('collapsed');
+		} else {
+			container.style.maxHeight = container.scrollHeight + 'px';
+			button.innerHTML = 'Згорнути текст';
+			button.classList.remove('collapsed');
+		}
+	});
 
-  button.addEventListener("click", function () {
-    if (container.style.maxHeight) {
-      container.style.maxHeight = null;
-      button.innerHTML = "Розгорнути текст";
-      button.classList.add("collapsed");
-    } else {
-      container.style.maxHeight = container.scrollHeight + "px";
-      button.innerHTML = "Згорнути текст";
-      button.classList.remove("collapsed");
-    }
-  });
+	// FOOTER SECTION акардион локации
 
-  // FOOTER SECTION акардион локации
+	$(document).ready(function() {
+		$('.accordion-header').click(function() {
+			if ($(this).closest('.header').length) {
+				var isAlreadyOpen = $(this).hasClass('active');
+				$('.header .accordion-header').not(this).removeClass('active').next('.accordion-content').slideUp();
+				if (!isAlreadyOpen) {
+					$(this).addClass('active').next('.accordion-content').slideDown();
+				} else {
+					$(this).removeClass('active').next('.accordion-content').slideUp();
+				}
+			} else {
+				var content = $(this).next('.accordion-content');
+				if ($(this).hasClass('active')) {
+					$(this).removeClass('active');
+					content.slideUp();
+				} else {
+					$('.accordion-header.active').not(this).removeClass('active');
+					$('.accordion-content').not(content).slideUp();
+					$(this).addClass('active');
+					content.slideDown();
+				}
+			}
+		});
+	});
 
-  $(document).ready(function () {
-    $('.accordion-header').click(function () {
-      if ($(this).closest('.header').length) {
-        var isAlreadyOpen = $(this).hasClass('active');
-        $('.header .accordion-header').not(this).removeClass('active').next('.accordion-content').slideUp();
-        if (!isAlreadyOpen) {
-          $(this).addClass('active').next('.accordion-content').slideDown();
-        } else {
-          $(this).removeClass('active').next('.accordion-content').slideUp();
-        }
-      } else {
-        var content = $(this).next('.accordion-content');
-        if ($(this).hasClass('active')) {
-          $(this).removeClass('active');
-          content.slideUp();
-        } else {
-          $('.accordion-header.active').not(this).removeClass('active');
-          $('.accordion-content').not(content).slideUp();
-          $(this).addClass('active');
-          content.slideDown();
-        }
-      }
-    });
-  });
+	// FOOTER SECTION список телефонов
+	$(document).ready(function() {
+		$('.click__address').click(function() {
+			$('.click__address-container').toggleClass('expanded');
+		});
+	});
 
-  // FOOTER SECTION список телефонов
-  $(document).ready(function () {
-    $('.click__address').click(function () {
-      $('.click__address-container').toggleClass('expanded');
-    });
-  });
+	// Popup
+	$(document).ready(function() {
+		$('#popup').fadeIn();
+		$('.popup__btn, #popup').click(function(e) {
+			if ($(e.target).hasClass('popup__btn')) {
+				closePopup();
+			} else if ($(e.target).attr('id') === 'popup') {
+				closePopup();
+			}
+		});
 
-  // Popup
-  $(document).ready(function() {
-    $('#popup').fadeIn();
-    $('.popup__btn, #popup').click(function(e) {
-        if ($(e.target).hasClass('popup__btn')) {
-            closePopup();
-        } else if ($(e.target).attr('id') === 'popup') {
-            closePopup();
-        }
-    });
+		function closePopup() {
+			$('#popup').css('right', '-1000%');
+			$('.body').removeClass('popup-open');
+			$('body').removeClass('popup-open');
+		}
 
-    function closePopup() {
-        $('#popup').css('right', '-1000%');
-        $('.body').removeClass('popup-open');
-        $('body').removeClass('popup-open');
-    }
-
-    setTimeout(function() {
-        $('#popup').css('right', '0');
-        $('.body').addClass('popup-open');
-        $('body').addClass('popup-open');
-    }, 2000);
-});
+		setTimeout(function() {
+			$('#popup').css('right', '0');
+			$('.body').addClass('popup-open');
+			$('body').addClass('popup-open');
+		}, 2000);
+	});
 
 }
--- src/js/files/main.js
+++ src/js/files/main.js
@@ -1,6 +1,10 @@
 export function main() {
-	initSwiper()
-
+	initSwiper();
+	initLangBox();
+	initMobileMenu();
+	initMessage();
+	initPhoneBoxes();
+	hideFixedHeader();
 
 	function initSwiper() {
 		let swiperWaterCare = new Swiper('.slider-water-care', {
@@ -17,7 +21,7 @@ export function main() {
 				clickable: true
 
 			}
-		})
+		});
 		let swiperHero = new Swiper('.slider-hero', {
 			slidesPerView: 1,
 			loop: false,
@@ -31,7 +35,99 @@ export function main() {
 				el: '.slider-hero__nav .swiper-pagination',
 				clickable: true
 			}
-		})
+		});
 	}
+
+	function initLangBox() {
+		let langCurrentElements = document.querySelectorAll('.lang__current');
+		langCurrentElements.forEach((langCurrent, index) => {
+			let langBox = document.querySelectorAll('.lang__text-holder')[index];
+			langCurrent.addEventListener('click', () => {
+				langCurrent.classList.toggle('active');
+				langBox.classList.toggle('active');
+			});
+		});
+	}
+
+	function updateMenuPosition() {
+		let menu = document.querySelector('.header__menu');
+		let fixHeader = document.querySelector('.fixed-header');
+		if (menu && fixHeader && menu.classList.contains('active')) {
+			let heightFixHeader = fixHeader.offsetHeight;
+			menu.style.top = `${heightFixHeader}px`;
+		} else if (menu) {
+			menu.style.top = '';
+		}
+	}
+
+	function initMobileMenu() {
+		let menuButton = document.querySelector('.header__menu-button');
+		let menu = document.querySelector('.header__menu');
+
+		menuButton.addEventListener('click', () => {
+			document.body.classList.toggle('lock');
+			if (menuButton.classList.contains('_icon-List')) {
+				menuButton.classList.remove('_icon-List');
+				menuButton.classList.add('_icon-X');
+			} else {
+				menuButton.classList.add('_icon-List');
+				menuButton.classList.remove('_icon-X');
+			}
+			menu.classList.toggle('active');
+			updateMenuPosition();
+		});
+
+		// Добавляем обработчик события 'resize' к окну
+		window.addEventListener('resize', updateMenuPosition);
+	}
+
+	function initMessage() {
+		let messageBlock = document.querySelector('.message');
+		let messageClose = document.querySelector('.message__close');
+
+		messageClose.addEventListener('click', () => {
+			if (messageBlock) {
+				messageBlock.classList.add('hide');
+				updateMenuPosition();
+			}
+		});
+	}
+
+	function initPhoneBoxes() {
+		let phoneBoxes = document.querySelectorAll('.contacts__phone.phone-box');
+
+		// Проходим по каждому контейнеру phone-box
+		phoneBoxes.forEach((phoneBox) => {
+			let phoneItem = phoneBox.querySelector('.phone-box__phone.contacts-link._icon-CaretDown');
+			let phoneList = phoneBox.querySelector('.phone-box__list');
+
+			if (phoneItem && phoneList) {
+				phoneItem.addEventListener('mouseenter', () => {
+					phoneList.classList.add('show');
+				});
+				phoneList.addEventListener('mouseleave', () => {
+					phoneList.classList.remove('show');
+				});
+			}
+		});
+	}
+
+	function hideFixedHeader() {
+		if (window.matchMedia('(min-width: 1280px)').matches) {
+			let fixedHeader = document.querySelector('.fixed-header');
+			let lastScrollTop = 0;
+			window.addEventListener('scroll', function() {
+				let currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+				if (currentScroll > lastScrollTop) {
+					fixedHeader.style.top = '-100%';
+				} else {
+					fixedHeader.style.top = '0';
+				}
+				lastScrollTop = currentScroll <= 0 ? 0 : currentScroll;
+			}, false);
+
+		}
+	}
+
 
 }
